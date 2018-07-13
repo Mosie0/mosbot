8e65ed8b1ef815085ee89d788ad2234f8ab0d829
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2018-07-13 13:47:57 -0700

    asd

--- commands/Info/roles.js
+++ commands/Info/roles.js
@@ -1,15 +1,5 @@
-const Discord = require("discord.js");
-
 module.exports.run = async (bot, message, args) => {
-    let sIcon = message.guild.iconURL;
-    let serverEmbed = new Discord.RichEmbed()
-        .setColor("#000FF")
-        .addField("Server Roles", message.guild.roles.map(role => role.name).join(' , '))
-        .setAuthor("Server Owner" + `\n` + message.guild.owner.user.username, message.guild.owner.user.avatarURL)
-        .setThumbnail(sIcon)
-    message.channel.startTyping();
     message.channel.send(`Server Roles ${message.guild.roles.map(role => role.name).join(' , ')}`);
-    await message.channel.stopTyping();
 }
 
 module.exports.help = {
