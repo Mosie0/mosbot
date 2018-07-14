ce6d5f0c1ebc0f2cb17c8bdc9fa85b88bb9d981b
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2018-07-14 08:01:18 -0700

    ewwasdas

--- commands/BotOwner/MosieCommands/restart.js
+++ commands/BotOwner/MosieCommands/restart.js
@@ -8,16 +8,10 @@ module.exports.run = async (bot, message, args) => {
     .setThumbnail(bot.user.avatarURL)
     .setFooter("Command Ran By: " + message.author.username, message.author.avatarURL)
     .setTimestamp()
-    const cantfindembed = new Discord.RichEmbed()
-        .setColor("#FF0000")
-        .setDescription(`Can't Find the **modlogs** Channel to Post my Restarting Message! **${bot.user.username}** Can't be Restart without that Channel!`)
-        .setFooter("Command Ran By: " + message.author.username, message.author.avatarURL);
-    let alert = message.guild.channels.find('name', "modlogs");
-    if (!alert) return message.channel.send(cantfindembed);
     console.log(`Bot Has Been Restart By: ` + message.author.username);
     if (message.author.id !== "283311727477784576") return;
     await message.react("✅");
-    await alert.send(botembed);
+    await message.channel.send(botembed);
     message.delete(1000).catch();
     fs.writeFile('./log.json', JSON.stringify(`Bot has Been Restarted.`), (err) => {
         if (err) console.log(err)
