7fe90060674c7c25e0ab602a2f0ea10dddf38134
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2018-07-14 14:29:28 -0700

    asd

--- commands/Info/PartnerChief.js
+++ commands/Info/PartnerChief.js
@@ -16,7 +16,7 @@ Features:
 
 `)
 .addField(`Server Invite`, `https://discord.gg/ZMYu7KK`)
-.setthumbnail(`https://cdn.discordapp.com/avatars/288450828837322764/a_9da0205f2e4a0f15565046ad156ac839.gif`)
+.setThumbnail(`https://cdn.discordapp.com/avatars/288450828837322764/a_9da0205f2e4a0f15565046ad156ac839.gif`)
 message.channel.send(embed);
 }
 module.exports.help = {
