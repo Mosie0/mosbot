572bf47774ad70c2cc5b3925398ae0ee02068cc4
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2018-07-14 13:39:57 -0700

    Update

--- commands/Info/jonbn123 partnership info.js
+++ commands/Info/jonbn123 partnership info.js
@@ -13,7 +13,7 @@ Jonbn123's Discord:
 》💥 Q&A's
 `)
 .addField(`Server Invite`, `https://discord.gg/SeWwhDd`)
-.setthumbnail(https://images-ext-2.discordapp.net/external/l27Gxb4vbIwafwJ1QCI3KHb32bui1NvYmV-SQMgmDgA/https/discordapp.com/api/guilds/251055654465175554/icons/f2d1864625cf8d7e5865c9af98f827ca.jpg)
+.setthumbnail(`https://images-ext-2.discordapp.net/external/l27Gxb4vbIwafwJ1QCI3KHb32bui1NvYmV-SQMgmDgA/https/discordapp.com/api/guilds/251055654465175554/icons/f2d1864625cf8d7e5865c9af98f827ca.jpg`)
 message.channel.send(embed);
 }
 module.exports.help = {
