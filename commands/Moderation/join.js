3d472796792502181581f6bdae2925ff4d2f4a77
Mosie0 <39349787+Mosie0@users.noreply.github.com>
2018-07-15 20:46:50 -0700

    Create join.js

---
+++ commands/Moderation/join.js
@@ -0,0 +1,10 @@
+client.on('guildMemberAdd', member => {
+const embed = new Discord.RichEmbed()
+.setTitle("User Joined")
+.addField("User",member.tag,true)
+.addField("ID",member.id,true)
+.setThumbnail(member.iconUrl)
+.setTimestamp()
+.setFooter(member.guild.name)
+  client.channels.get('modlog').send(embed)
+});
