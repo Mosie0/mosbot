5b651e7216db5d39b08a4cd8977b82e63cc2ea02
Mosie0 <39349787+Mosie0@users.noreply.github.com>
2018-07-16 10:43:46 -0700

    Update StaffList.js

--- commands/Info/StaffList.js
+++ commands/Info/StaffList.js
@@ -1,4 +1,3 @@
-
 const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
@@ -6,10 +5,11 @@ if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply("Sorry 
 const embed = new Discord.RichEmbed()
 .setColor(`#11a8ff`)
 .addField(`Owner`, `<@283311727477784576>`)
-.addField(`Co-Owner`, `N/A`)
-.addField(`Head Admin`, `<@244271175608303616>`)
-.addField(`Admins`, `<@289148183282712590>, <@297871529621061643>, <@434866964557463595>, <@298201869585612810>, <@330752829859954698>, <@421033482752360448>`)
-.addField(`Moderators`, `<@265344329147613185>, <@386620953414729728>, <@281153525977972736>, <@308290036334460929>, <@362143129136267265>, <@372468580643635211>, <@203259894743302145>`)
+.addField(`<@&446172124353265665>`, `N/A`)
+.addField(`<@&447962800791355402>`, `<@288450828837322764>`)
+.addField(`<@&467779026069618694>`, `<@244271175608303616>`)
+.addField(`<@&431811267968106506>`, `<@289148183282712590>, <@297871529621061643>, <@434866964557463595>, <@298201869585612810>, <@330752829859954698>, <@421033482752360448>`)
+.addField(`<@&422058000250044416>`, `<@265344329147613185>, <@386620953414729728>, <@281153525977972736>, <@308290036334460929>, <@362143129136267265>, <@372468580643635211>, <@203259894743302145>`)
 .setThumbnail(`https://cdn.discordapp.com/attachments/421895390065852431/468466969637552159/GamingHQLogo.png`)
 .setImage(`https://cdn.discordapp.com/attachments/422106448487776276/459126100061061139/my_tweet_7.png`)
 message.channel.send(embed);
@@ -17,3 +17,4 @@ message.channel.send(embed);
 module.exports.help = {
     name: "StaffList"
 }
+
