a019d280f89991e7fc6302251b65d7f771c6a3b2
Mosie0 <39349787+Mosie0@users.noreply.github.com>
2018-07-15 19:19:25 -0700

    Update Rules.js

--- commands/Info/Rules.js
+++ commands/Info/Rules.js
@@ -3,8 +3,6 @@ const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
 const embed = new Discord.RichEmbed()
 .setColor(`#11a8ff`)
-.setImage(`https://cdn.discordapp.com/attachments/422052478075535360/459426338823077898/my_tweet_8.png`)
-.addField(`Welcome to ***Gaming HQ!***`, `We are a bunch of people that just love to have fun, we have many fun things that you can enjoy in this discord! With all of us, it creates FUN!`)
 .setImage(`https://cdn.discordapp.com/attachments/422052478075535360/459426688212664330/my_tweet_4.png`)
 .addField(`
 ---------------------------Rules---------------------------
