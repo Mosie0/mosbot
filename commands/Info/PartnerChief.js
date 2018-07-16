f4d6b7684fbee6a93e4b1f33d44c804220833ab1
Mosie0 <39349787+Mosie0@users.noreply.github.com>
2018-07-16 12:09:42 -0700

    Update PartnerChief.js

--- commands/Info/PartnerChief.js
+++ commands/Info/PartnerChief.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
+if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply("Sorry but you don't have the permissions to use this command.");
 const embed = new Discord.RichEmbed()
 .setColor(`#11a8ff`)
 .setAuthor(`Owner: SUPERCHIEFYT`, `https://cdn.discordapp.com/avatars/288450828837322764/a_9da0205f2e4a0f15565046ad156ac839.gif`)
