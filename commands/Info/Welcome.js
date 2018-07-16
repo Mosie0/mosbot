9df28dce204d63f5b77c8fa5957efe02ea6ed597
Mosie0 <39349787+Mosie0@users.noreply.github.com>
2018-07-16 12:11:18 -0700

    Update Welcome.js

--- commands/Info/Welcome.js
+++ commands/Info/Welcome.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
+if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply("Sorry but you don't have the permissions to use this command.");
 const embed = new Discord.RichEmbed()
 .setColor(`#11a8ff`)
 .setImage(`https://cdn.discordapp.com/attachments/422052478075535360/459426338823077898/my_tweet_8.png`)
