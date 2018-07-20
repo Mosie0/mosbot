4732907947133b6967e98ee62ac53c6354445657
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2018-07-19 17:14:30 -0700

    Update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const fs = require("fs");
-const bot = new Discord.Client();
+const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
 let cooldown = new Set();
 let cdseconds = 2;
