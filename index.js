3c78bb6de777c68a5c7ac2fe671d2727d177bc60
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2018-08-01 19:13:32 -0700

    Updated Bot Owner Commands etc.

--- index.js
+++ index.js
@@ -157,23 +157,6 @@ fs.readdir("./commands/BotOwner", (err, files) => {
         bot.commands.set(props.help.name, props);
     });
 });
-
-
-fs.readdir("./commands/BotOwner/SuperCommands", (err, files) => {
-
-    if (err) console.log(err);
-    let jsfile = files.filter(f => f.split(".").pop() === "js");
-    if (jsfile.length <= 0) {
-        console.log("Couldn't find BotOwner/SuperCommands commands.");
-        return;
-    }
-
-    jsfile.forEach((f, i) => {
-        let props = require(`./commands/BotOwner/SuperCommands/${f}`);
-        console.log(`${f} loaded!`);
-        bot.commands.set(props.help.name, props);
-    });
-});
 fs.readdir("./commands/BotOwner/MosieCommands", (err, files) => {
 
     if (err) console.log(err);
