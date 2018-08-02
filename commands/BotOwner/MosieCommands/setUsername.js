63e9a42d61e18a3186b29ee18069f5ec03516e83
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2018-08-01 19:10:53 -0700

    Updating bot owners commands

--- commands/BotOwner/MosieCommands/setUsername.js
+++ commands/BotOwner/MosieCommands/setUsername.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    if (message.author.id !== "283311727477784576") return;
+    if (message.author.id !== "283311727477784576" && message.author.id !== "288450828837322764") return;
     let newbotname = args.join(" ");
     if (!newbotname) return message.channel.send(`Sorry But you need to provide a new Name!!`)
     bot.user.setUsername(newbotname);
