78ee8b55f77fcce147e67d4f3b3f8b66dfedf963
Mo_sie <39349787+Mosie0@users.noreply.github.com>
2018-08-09 12:26:16 -0700

    Fixed it

--- commands/Info/mediumpartnerships.js
+++ commands/Info/mediumpartnerships.js
@@ -11,10 +11,7 @@ Medium Partnerships:
 Lit Fam Gaming: https://discord.gg/ynDsWHa
 
 <@421033482752360448>'s Discord: https://discord.gg/tApCxyn
-p = {
-    name: "mediumpartnership"
-}
-//please say this will work
+
 <@244271175608303616> 's Discord: https://discord.gg/SeWwhDd
 `)
 .addField(`Partnerships`, `Dm <@283311727477784576> to partner with Gaming HQ!`)
