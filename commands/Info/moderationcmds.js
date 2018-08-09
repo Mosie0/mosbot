529c0e743118e3641169f0811007344a51953adb
Mo_sie <39349787+Mosie0@users.noreply.github.com>
2018-08-09 14:31:36 -0700

    Changed Purge.js to Prune.js

--- commands/Info/moderationcmds.js
+++ commands/Info/moderationcmds.js
@@ -5,6 +5,17 @@ module.exports.run = async (bot, message, args) => {
         .setColor("#000FF")
         .setDescription("This is the Help Information")
         .addField(`Moderation Comammnds`, `Currently Working on it.`)
+        .setDescription(`m!addrole: Add a role with this command
+        m!ban: Ban a member with this command
+        m!dm: Dm a member with this command
+        m!kick: Kick with this command
+        m!mute: Mute with this command
+        m!purge: Purge messages with this command
+        m!removerole: Remove roles with this command
+        m!report: Report members with this command
+        m!say: Make the bot say something with this command
+        m!unban: Unban a member with this comamnd
+        m!warn: Warn a member with this command`)
         .setThumbnail(bot.user.displayAvatarURL)
         .setTimestamp()
         .setFooter("Command Ran By: " + message.author.username, message.author.avatarURL)
