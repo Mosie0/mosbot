879d4bc4c87aa434a842a4b2eb0fea353d7ca64c
Mo_sie <39349787+Mosie0@users.noreply.github.com>
2018-08-09 18:35:37 -0700

    fixed a command name

--- commands/Info/botinfo.js
+++ commands/Info/botinfo.js
@@ -13,7 +13,7 @@ module.exports.run = async (bot, message, args) => {
         .addField("Bot Name", "<@" + `${bot.user.id}` + ">", true)
         .addField("Bot Invite Link", `[Bot Invite](https://discordapp.com/oauth2/authorize?client_id=${bot.user.id}&permissions=8&scope=bot)`, true)
         .addBlankField()
-        .addField("To see other commands do", "**m!help**", true)
+        .addField("To see other commands do", "**m!commandshelp**", true)
         .addField("Created On", "**July 13th 2018**", true)
         .addField("Servers In", string)
         .setFooter("Creator of the Bot: SUPERCHIEFYT & Mo_sie");
