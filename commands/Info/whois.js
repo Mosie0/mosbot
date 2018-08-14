5065fb5c7d1d6bbc3de89150e12ca25a0576909f
Mo_sie <39349787+Mosie0@users.noreply.github.com>
2018-08-14 14:13:44 -0700

    whois command

--- commands/Info/whois.js
+++ commands/Info/whois.js
@@ -1,20 +1,21 @@
 const Discord = require("discord.js");
-
+const moment = require('moment-timezone');
 module.exports.run = async (bot, message, args) => {
-    let aTaged = message.mentions.users.first();
-    if (!aTaged) {
-        return message.reply(`Please Provide a User :wink:` + "\n **s!whois @user**");
-}
+    let aTaged = message.mentions.users.first() || message.author;
         let botembed = new Discord.RichEmbed()
         .setColor("#000FF")
         .setThumbnail(`${aTaged.displayAvatarURL}`)
+        .setAuthor(aTaged.tag)
         .addField("Name", `<@${aTaged.id}>`, true)
-        .addField("USERID", aTaged.id, true)
-        .addField("Status", aTaged.presence.status, true)
-        .setAuthor(`${aTaged.aTagedname}`, `${aTaged.displayAvatarURL}`)
-        .setFooter("Joined At" + aTaged.createdAt)
-        .setTimestamp(aTaged.createdAt)
-    message.channel.send(botembed);
+        .addField(`Nitro/Partner`, `${aTaged.avatar.startsWith('a_') ? 'Has Nitro or Partner' : 'Regular User'}`, true)
+        .addField("User ID", aTaged.id, true)
+        .addField(`Account Creation:`, `${moment(aTaged.createdAt).tz('America/Chicago').format('dddd, MMMM Do YYYY, h:mm:ss a zz')}`)
+        .addField(`Joined Server At:`, `${moment(aTaged.joinedAt).tz('America/Chicago').format('dddd, MMMM Do YYYY, h:mm:ss a zz') }`)
+        .addField("Status", aTaged.presence.status.toUpperCase(), true)
+        .addField(`Game`, aTaged.presence.game ? aTaged.presence.game.name: 'N/A', true)
+        .setTimestamp()
+    message.channel.send(botembed)
+    
 }
 module.exports.help = {
     name: "whois"
