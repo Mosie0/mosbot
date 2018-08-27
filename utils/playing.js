5a0441dc46fcc4d43a2be536a9e1dbd78c317786
RHG <huskey.rusty@gmail.com>
2018-08-26 21:00:05 -0400

    oo an new file

---
+++ utils/playing.js
@@ -0,0 +1,36 @@
+const pak = require("../package.json");
+
+// • ⦠ ⧐ ⧽
+
+module.exports = (bot) => {
+	//bot.user.setGame(`${config.default.prefix}help`);
+	bot.user.setActivity(`code`, { type: "STREAMING", url: "https://www.twitch.tv/rhg_discord" });
+
+	var status = [
+    `V${pak.version}`,
+  
+		`${bot.guilds.array().length} server${bot.guilds.array().length > 1 ? 's' : ''}`,
+		`${bot.channels.array().length} channel${bot.channels.array().length > 1 ? 's' : ''}`,
+		`${bot.channels.array().length} channel${bot.channels.array().length > 1 ? 's' : ''}`,
+    
+		`for lives`,
+	];
+
+	var types = [
+    `WATCHING`,
+		`LISTENING`,
+		`LISTENING`,
+    `LISTENING`,
+
+		`WATCHING`,
+	];
+
+	gameval = 0;
+	setInterval(() => {
+		if (gameval == status.length) {
+			gameval = 0;
+			}
+		bot.user.setActivity(`${status[gameval]}`, { type: types[gameval], url: "https://www.twitch.tv/twitch" });
+		gameval++;
+	}, 16000);
+};
