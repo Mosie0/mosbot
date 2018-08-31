110db0299f4ed2d774c507ef4f66225bf371fda7
Mo_sie <39349787+Mosie0@users.noreply.github.com>
2018-08-30 19:03:24 -0700

    changed it to random color, cause why not

--- commands/Fun/Mosie.js
+++ commands/Fun/Mosie.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     let botembed = new Discord.RichEmbed()
-        .setColor("#800080")
+        .setColor("RANDOM")
         .setImage("https://cdn.discordapp.com/attachments/444028025932349441/445662475665408010/tenor_6.gif");
 
 
