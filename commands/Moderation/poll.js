399acc075d7bb48cf4a956885da55d79e1db951f
Mo_sie <39349787+Mosie0@users.noreply.github.com>
2018-08-31 18:36:44 -0700

    little color change

--- commands/Moderation/poll.js
+++ commands/Moderation/poll.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
     if (!args.slice(1).join(' ')) return message.channel.send(`Please Provide Something for the Poll!`)
     message.delete().catch()
     const embed = new Discord.RichEmbed()
-        .setColor(`#000FF`)
+        .setColor(`RANDOM`)
         .setDescription(args.slice(1).join(' '))
         .setTitle(`Poll Created By ${message.author.username}`)
         .setFooter(`React to Vote!`)
