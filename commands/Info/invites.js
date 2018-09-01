6e9b41dc077a3f6be0c2ed12f5030d0b33a04a82
Mo_sie <39349787+Mosie0@users.noreply.github.com>
2018-09-01 10:43:04 -0700

    oo top invites

--- commands/Info/invites.js
+++ commands/Info/invites.js
@@ -1,40 +1,23 @@
+
 const Discord = require('discord.js'),
-    arraySort = require('array-sort');
-   
+arraySort = require('array-sort'),
+table = require('table'); 
 module.exports.run = async (bot, message, args) => {
-  message.guild.fetchInvites().catch(error => {
-        return message.channel.send('Sorry, I don\'t have the proper permissions to view invites!');
-        }).then((invites) => {
-        invites = invites.array();
-        arraySort(invites, 'uses', {
-            reverse: true
-        });
-
-        var possibleInvites = [];
-        let output = ``;
-       // possibleInvites.push([`User  ::  Invites  ::  Code`]);
-        invites.forEach(function (invite) {
-            if (invite.uses != 0) {
-                possibleInvites.push(invite.inviter.username);
-            }
-        });
-
-        const longest = possibleInvites.reduce((long, str) => Math.max(long, str.length), 0);
-        invites.forEach(function (invite){
-            if (invite.uses != 0) {
-                output += `${invite.inviter.username}${" ".repeat(longest - invite.inviter.username.length)} :: ${invite.uses}\n`;
-            }
-        });
-
-        const embed = new Discord.RichEmbed()
-            .setAuthor(`Invite Leaderboard`)
-            .setColor(0x36393E)
-            .setDescription(`\`\`\`asciidoc
-${output}\`\`\``);
-
-        message.channel.send({embed});
-    });
+let invites = await message.guild.fetchInvites().catch(error => { 
+    return message.channel.send('Sorry, I don\'t have the proper permissions to view invites!');
+})
+invites = invites.array();
+arraySort(invites, 'uses', { reverse: true });
+let possibleInvites = [['User', 'Uses']]; 
+invites.forEach(function (invite) {
+    possibleInvites.push([invite.inviter.username, invite.uses]);
+})
+const embed = new Discord.RichEmbed()
+    .setColor('RANDOM')
+    .addField('Leaderboard', `${table.table(possibleInvites)}`);
+message.channel.send(embed)
 }
 module.exports.help = {
-    name: "invites"
+name: "topinvites",
+names: "TopInvites"
 }
