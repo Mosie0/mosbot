b8ee42cbb5e0b60a4bf0a4fdf11747a7d015b189
Mo_sie <39349787+Mosie0@users.noreply.github.com>
2018-09-04 20:49:32 -0700

    something o.O

--- commands/Info/help.js
+++ commands/Info/help.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
     let botembed = new Discord.RichEmbed()
         .setColor("RANDOM")
         .setDescription("This is the Command List")
-        .addField(`Commands List`, `Bot Owner Commands: m!botownercmds \n Fun Commands: m!funcmds \n Moderation Commands: m!moderationcmds`)
+        .addField(`Commands List`, `Bot Owner Commands: m!botownercmds \n Fun Commands: m!funcmds \n Moderation Commands: m!moderationcmds \n Information Commands: m!infocmds`)
         .setThumbnail(bot.user.displayAvatarURL)
         .setTimestamp()
         .setFooter("Command Ran By: " + message.author.username, message.author.avatarURL)
