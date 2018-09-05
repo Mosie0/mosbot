ac5c3d5edde4badf43882c438657317fe795973c
Mo_sie <39349787+Mosie0@users.noreply.github.com>
2018-09-04 19:44:30 -0700

    moderation commds list :P

--- commands/Info/moderationcmds.js
+++ commands/Info/moderationcmds.js
@@ -5,17 +5,26 @@ module.exports.run = async (bot, message, args) => {
         .setColor("#000FF")
         .setDescription("This is the Help Information")
         .setTitle(`Moderation Commands`)
-        .setDescription(`m!addrole: Add a role with this command
-        m!ban: Ban a member with this command
-        m!dm: Dm a member with this command
-        m!kick: Kick with this command
-        m!mute: Mute with this command
-        m!purge: Purge messages with this command
-        m!removerole: Remove roles with this command
-        m!report: Report members with this command
-        m!say: Make the bot say something with this command
-        m!unban: Unban a member with this comamnd
-        m!warn: Warn a member with this command`)
+        .setDescription(`m!addrole: Add a role
+        m!ban: Ban a member
+        m!dm: Dm a member
+        m!kick: Kick a member
+        m!mute: Mute a member
+        m!purge: Purge messages
+        m!removerole: Remove a role
+        m!report: Report members
+        m!say: Make the bot say something
+        m!unban: Unban a member
+        m!warn: Warn a member
+        m!id: Find a members user ID
+        m!poll: Start a poll
+        m!softban:
+        m!support: File a support ticket
+        m!lock: Lock this channel, so no one can chat in it
+        m!unlock: Allow the role @ everyone to chat in that channel
+        m!discordupdates: Allows the bot to send a message to the channel #change-log
+        m!annoucement: Send an embedded message to the #annoucements channel
+        m!whois: Gives brief data on a member`)
         .setThumbnail(bot.user.displayAvatarURL)
         .setTimestamp()
         .setFooter("Command Ran By: " + message.author.username, message.author.avatarURL)
