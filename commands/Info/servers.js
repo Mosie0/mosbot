571936b8d0e36faf0e951d079de5d51660384f3e
Mo_sie <39349787+Mosie0@users.noreply.github.com>
2018-09-07 07:21:21 -0700

    these ones :P

--- commands/Info/servers.js
+++ commands/Info/servers.js
@@ -1,17 +1,12 @@
-const { RichEmbed } = require('discord.js');
-const hastebin = require('hastebin-gen');
-module.exports.run = async (bot, message, args, ops) => {
-    if (message.author.id !== ops) return;
+const Discord = require('discord.js')
+module.exports.run = async (bot, message, args) => {
     let string = '';
-    bot.guilds.forEach(guild => { string += `Server Name: (${guild.name})\nServer ID: (${guild.id})\nServer Icon: (${guild.iconURL}\n Member Count ${guild.memberCount})` + '\n\n'; })
-    hastebin(string, "js").then(r => {
-        var hastLink = r
-        const hastEmb = new RichEmbed()
-            .setColor(`RANDOM`)
-            .setTitle(`Here is a List of all of the Servers: Current Number of Servers **${bot.guilds.size}**`)
-            .addField('Link: ', `${hastLink}`)
-        message.channel.send(hastEmb)
-    })
+    bot.guilds.forEach(guild => { string += guild.name + '\n'; })
+    let embed = new Discord.RichEmbed()
+    .setColor(`#0000FF`)
+    .setTitle(`What servers are MosBot in?`)
+    .setDescription(string)
+    message.channel.send(embed)
 }
 module.exports.help = {
     name: "servers"
