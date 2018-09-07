ead6b1770828001e28af0dc7651aed3a9b35c84a
Mo_sie <39349787+Mosie0@users.noreply.github.com>
2018-09-07 08:17:08 -0700

    prob gonna crash this time

--- commands/Moderation/testing.js
+++ commands/Moderation/testing.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 const embed = new Discord.RichEmbed()
   .setColor("RANDOM")
-  
+  .setDescription(embed)
 module.exports.run = async (bot, message, args) => {
 message.channel.send(embed).then(sentmessage => {
   embed.setDescription("new Decription");
