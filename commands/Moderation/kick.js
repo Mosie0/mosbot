c52fc4335c5cc9aaba8f9fc2ede52c186f4e4acf
RHG <huskey.rusty@gmail.com>
2018-09-06 22:49:55 -0400

    Update kick.js

--- commands/Moderation/kick.js
+++ commands/Moderation/kick.js
@@ -7,14 +7,16 @@ module.exports.run = async (bot, message, args) => {
     let kReason = args.join(" ").slice(22);
     if (!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("Sorry but you don't have the Kick Memebrs Permission");
     if (kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person has Staff Permissions in the server!!");
-    let kickembed = new Discord.RichEmbed()
+    
+	let kickembed = new Discord.RichEmbed()
     .setColor("#0011FF")
     .setDescription(`Member Kicked`)
     .setFooter(kUser.id)
     .addField(`Kicked User`, kUser, true)
     .addField(`Moderator`, `<@${message.author.id}>`, true)
     .addField(`Channel`, message.channel, true)
-    .setTimestamp()
+    .setTimestamp();
+    
     Settings.findOne({serverID: message.channel.guild.id}, (err, settings) => {
         if (err) console.log(err);
         if (settings) {
