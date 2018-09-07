2cefd30ec453d4a8a459ff64358c1e6da9b7e55a
Mo_sie <39349787+Mosie0@users.noreply.github.com>
2018-09-07 08:33:03 -0700

    hM

--- commands/Moderation/testing.js
+++ commands/Moderation/testing.js
@@ -6,6 +6,8 @@ const embed = new Discord.RichEmbed()
 module.exports.run = async (bot, message, args) => {
 message.channel.send(embed).then(sentmessage => {
   embed.setDescription("new Decription");
+  message.channel.send(embed).then(sentmessage => {
+    embed.setDescription("new Decriptionnew Decriptionnew Decription");
 
   sentmessage.edit(embed)
 });
