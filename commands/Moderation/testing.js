560d52d6770e986f24f694d9a41f6372d8ac25d3
Mo_sie <39349787+Mosie0@users.noreply.github.com>
2018-09-07 08:10:59 -0700

    prob gonna crash boT

--- commands/Moderation/testing.js
+++ commands/Moderation/testing.js
@@ -1,4 +1,13 @@
+const Discord = require('discord.js');
+
+
 module.exports.run = async (bot, message, args) => {
+message.channel.send(embed).then(sentmessage => {
+  embed.setDescription("new Decription");
+
+  sentmessage.edit(embed)
+});
+
 
 }
 module.exports.help = {
