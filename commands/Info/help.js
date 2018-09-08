6cf04459997b9ec0e439a8f3531a4c6a914ebdf6
Mo_sie <39349787+Mosie0@users.noreply.github.com>
2018-09-07 21:18:40 -0700

    woaH thaNKS RHG!

--- commands/Info/help.js
+++ commands/Info/help.js
@@ -1,16 +1,96 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js'),
+    pageemo = ["🏠", "🛠", "🎉", "❔", "🔠", "🎲", "❌"];
 
-module.exports.run = async (bot, message, args) => {
-    let botembed = new Discord.RichEmbed()
+
+const pages = [
+	{
+		title: "Help Menu",
+		description: `
+${pageemo[0]} to return **Home**.
+
+${pageemo[1]} for **Moderation Commands**.
+
+${pageemo[2]} for **Fun Commands**.
+
+${pageemo[3]} for **Information Commands**.
+
+${pageemo[4]} for **Bot Owner Commands**.
+
+${pageemo[5]} for **Credits**.
+
+${pageemo[6]} to exit the menu.`
+	},
+	{
+		title: "Moderation Commands",
+		description: `
+YOYOYOYO THIS IS A TEST`,
+	},
+	
+	{
+		title: "Fun Commands",
+		description: `
+YOYOYOYO THIS IS A **TEST2**`,
+	},
+	
+    	{
+		title: "Information Commands",
+		description: `
+**YOYOYOYO** THIS IS A TEST3`,
+	},
+	
+	{
+		title: "Bot Owner Commands",
+		description: `
+**YOYOYOYO** THIS IS A TEST4`,
+	},
+	
+	{
+		title: "Credits",
+		description: `
+**YOYOYOYO** THIS IS A TEST5`,
+	},
+]
+let page = 1; 
+
+module.exports.run = (bot, message, args) => {
+    message.delete(500).catch();
+    let embed = new Discord.RichEmbed()
         .setColor("RANDOM")
-        .setDescription("This is the Command List")
-        .addField(`Commands List`, `Bot Owner Commands: m!botownercmds \n Fun Commands: m!funcmds \n Moderation Commands: m!moderationcmds \n Information Commands: m!infocmds`)
-        .setThumbnail(bot.user.displayAvatarURL)
-        .setTimestamp()
-        .setFooter("Command Ran By: " + message.author.username, message.author.avatarURL)
-    message.channel.send(botembed);
-};
+        .setTitle(pages[0].title)
+        .setDescription(pages[0].description);
 
+    message.channel.send(embed).then(msg => {
+	function reactArrows(arrow) {
+		if (arrow === 7) return;
+		msg.react(pageemo[arrow]).then(_ => {
+			reactArrows(arrow + 1);
+		}).catch(e => console.error(`Reaction Error: ${e}`));
+	}
+	function handleReaction(reaction) {
+		// console.log(`${reaction.emoji.name} from ${reaction.users.last().username}`);
+		reaction.remove(reaction.users.last()).catch(e => {
+		    if (e.code === 50013) reaction.message.channel.send("I need the 'Manage Messages' permission in order to work properly!");
+		});
+		const rid = pageemo.indexOf(reaction.emoji.name);
+		if (rid !== 6) {
+			let embed2 = new Discord.RichEmbed()
+			.setColor("RANDOM")
+			.setTitle(pages[rid].title)
+			.setDescription(pages[rid].description)
+
+			msg.edit(embed2)
+		} else { 
+			msg.delete(1000)
+		}
+	    
+	}
+	reactArrows(0)
+	let collector = msg.createReactionCollector((reaction, user) => {return user.id !== msg.client.user.id && pageemo.includes(reaction.emoji.name);});
+        collector.on("collect", (reaction) => {
+            handleReaction(reaction);
+        }); 
+    });
+};
 
 module.exports.help = {
     name: "help"
