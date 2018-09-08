57225183c11e29dc3c6bb412aede23da540f54cf
Mo_sie <39349787+Mosie0@users.noreply.github.com>
2018-09-07 22:10:05 -0700

    moderation cmds testing

--- commands/Info/help.js
+++ commands/Info/help.js
@@ -23,7 +23,27 @@ ${pageemo[6]} to exit the menu.`
 	{
 		title: "Moderation Commands",
 		description: `
-YOYOYOYO THIS IS A TEST`,
+		m!addrole: Add a role
+        m!ban: Ban a member
+        m!dm: Dm a member
+        m!kick: Kick a member
+        m!mute: Mute a member
+        m!purge: Purge messages
+        m!removerole: Remove a role
+        m!report: Report members
+        m!say: Make the bot say something
+        m!unban: Unban a member
+        m!warn: Warn a member
+        m!id: Find a members user ID
+        m!poll: Start a poll
+        m!softban:
+        m!support: File a support ticket
+        m!lock: Lock this channel, so no one can chat in it
+        m!unlock: Allow the role @ everyone to chat in that channel
+        m!discordupdates: Allows the bot to send a message to the channel #change-log
+        m!annoucement: Send an embedded message to the #annoucements channel
+        m!whois: Gives brief data on a member
+        m!memberlist: Gives the list of members in a certain role`,
 	},
 	
 	{
