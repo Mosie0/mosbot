e05180b33921558dd5adda41fa7a01fb278927a5
Mo_sie <39349787+Mosie0@users.noreply.github.com>
2018-09-07 18:40:54 -0700

    testing

---
+++ commands/Info/helptesting.js
@@ -0,0 +1,63 @@
+const Discord = require('discord.js'),
+    backwardsemo =  "⏪",
+    forwardsemo = "⏩";
+
+
+let pages = ['testing! :)', 'Second page', 'Third', 'You can add pages', 'All you need to do is add another item in the array', '**Supports markdown and regular chat description properties**']; 
+let page = 1; 
+
+module.exports.run = (bot, message, args) => {
+    message.delete(500).catch();
+    const embed = new Discord.RichEmbed()
+        .setColor(0xffffff)
+        .setFooter(`Page ${page} of ${pages.length}`)
+        .setDescription(pages[page - 1]);
+
+    message.channel.send(embed).then(msg => {
+
+        msg.react(backwardsemo).then(r => {
+            msg.react(forwardsemo);
+
+            const backwardsFilter = (reaction, user) => reaction.emoji.name === backwardsemo && user.id === message.author.id;
+            const forwardsFilter = (reaction, user) => reaction.emoji.name === forwardsemo && user.id === message.author.id;
+
+            const backwards = msg.createReactionCollector(backwardsFilter, { time: 60000 });
+            const forwards = msg.createReactionCollector(forwardsFilter, { time: 60000 });
+
+
+            backwards.on('collect', r => {
+                if (page === 1) return;
+                msg.clearReactions().then(() => {
+                    page--;
+                    embed.setDescription(pages[page - 1]);
+                    embed.setFooter(`Page ${page} of ${pages.length}`);
+                    msg.edit(embed);
+                    msg.react(backwardsemo).then(() => {
+                        msg.react(forwardsemo);
+                    });
+                });
+            });
+
+            forwards.on('collect', r => {
+                if (page === pages.length) return;
+                msg.clearReactions().then(() => {
+                    page++;
+                    embed.setDescription(pages[page - 1]);
+                    embed.setFooter(`Page ${page} of ${pages.length}`);
+                    msg.edit(embed);
+                    msg.react(backwardsemo).then(() => {
+                        msg.react(forwardsemo);
+                    });
+                });
+            });
+
+            forwards.on('end', () => {
+                msg.delete();
+            });
+        });
+    });
+};
+
+module.exports.help = {
+    name: "help1"
+}
