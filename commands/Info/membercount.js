8c1803c22520586e2aef778ef73fad82fe81bcff
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2018-09-09 02:18:17 -0700

    fixed...

--- commands/Info/membercount.js
+++ commands/Info/membercount.js
@@ -1,17 +1,16 @@
 const Discord = require('discord.js');
-
-module.exports.run = async member => {
-    let serverSize = member.guild.memberCount;
-    let botCount = member.guild.members.filter(m => m.user.bot).size;
+module.exports.run = async (bot, message, args) => {
+    let serverSize = message.guild.memberCount;
+    let botCount = message.guild.members.filter(m => m.user.bot).size;
     let humanCount = serverSize - botCount;
-    let servercount = serverSize;
-    let membercount = new Discord.RichEmbed()
-.setColor("RANDOM")
-.addField(`Total Humans in the Discord`, `${humanCount}`)
-.addField(`Total Members`, `${servercount}`)
-    message.channel.send(membercount);
-};
-
+const embed = new Discord.RichEmbed()
+.setColor(`RANDOM`)
+.setTimestamp()
+.addField(`Members`, `**${serverSize}**`, true)
+.addField(`Humans`, `**${humanCount}**`, true)
+.addField(`Bots`, `**${botCount}**`, true)
+message.channel.send(embed)
+}
 module.exports.help = {
     name: "membercount",
     names: "mc"
