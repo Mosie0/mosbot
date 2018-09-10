beb1728c062b05643dd00f875b2e4b4a1d41e92f
Mo_sie <39349787+Mosie0@users.noreply.github.com>
2018-09-10 07:31:45 -0700

    hM

--- commands/Fun/testing2.js
+++ commands/Fun/testing2.js
@@ -2,11 +2,15 @@ const { Canvas } = require("canvas-constructor");
 
 module.exports.run = (bot, message, args) => {
     const serverSize = message.guild.memberCount;
+    const botCount = message.guild.members.filter(m => m.user.bot).size;
+    const humanCount = serverSize - botCount;
     const canvas = new Canvas(400, 200)
     .setColor("#000000")
     .addRect(0, 0, 400, 200)
     .setColor("#ffffff")
-    .addText(`Server Members: ${serverSize}`, 200, 100)
+    .addText(`Server Members: ${serverSize}`, 400, 100)
+    .addText(`Bot Count: ${botCount}`, 200, 100)
+    .addText(`Human Count: ${humanCount}`, 100, 100)
 
     message.channel.send({files: [{ attachment: canvas.toBuffer(), name: "Account.png"}]});
 }
