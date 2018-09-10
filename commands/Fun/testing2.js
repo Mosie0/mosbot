4f37c16d2d4c45f3cc153f5a7675705550af0450
Mo_sie <39349787+Mosie0@users.noreply.github.com>
2018-09-09 19:40:29 -0700

    inital test

---
+++ commands/Fun/testing2.js
@@ -0,0 +1,16 @@
+const { MessageAttachment } = require('discord.js'); 
+const { Canvas } = require("canvas-constructor");
+
+const canvas = new Canvas(400,187)
+let serverSize = message.guild.memberCount;
+let canvas = new Canvas(x, y)
+
+.addRect(serverSize, 200, 130)
+
+
+
+
+module.exports.help = {
+    name: "testing1",
+    names: "Testing1"
+}
