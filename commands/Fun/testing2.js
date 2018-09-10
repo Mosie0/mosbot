72f15e51caec14f04b671ceffbe8f6b45f3ac246
RHG <huskey.rusty@gmail.com>
2018-09-10 12:36:57 -0400

    Update testing2.js

--- commands/Fun/testing2.js
+++ commands/Fun/testing2.js
@@ -1,23 +1,27 @@
 const { Canvas } = require("canvas-constructor");
+const { get } = require("snekfetch");
 
 module.exports.run = (bot, message, args) => {
     const serverSize = message.guild.memberCount;
     const botCount = message.guild.members.filter(m => m.user.bot).size;
     const humanCount = serverSize - botCount;
-    const canvas = new Canvas(400, 200)
-    .setColor("#2C2F33")
-    .addRect(0, 0, 400, 200)
-    .setColor("#ffffff")
-    .setTextFont('25px Impact')
-    //.setTextAlign("center")
-    .addText(`Server Members`, 10, 15)
-    .setTextFont('20px Impact')
-    .addText(`Total Count: ${serverSize}`, 10, 75)
-    .addText(`Human Count: ${humanCount}`, 10, 100)
-    .addText(`Bot Count: ${botCount}`, 20, 125)
-    
+    get(message.guild.iconURL).then(guildIcon => {
+        const canvas = new Canvas(400, 200)
+        .setColor("#2C2F33")
+        .addRect(0, 0, 400, 200)
+        .setColor("#ffffff")
+        .setTextFont('25px Impact')
+        //.setTextAlign("center")
+        .addText(`Server Member Count`, 10, 25)
+        .setTextFont('20px Impact')
+        .addRoundImage(guildIcon, 250, 100, 78, 78, 64)
+        .addText(`Total Count: ${serverSize}`, 10, 100)
+        .addText(`Human Count: ${humanCount}`, 10, 125)
+        .addText(`Bot Count: ${botCount}`, 10, 150)
 
-    message.channel.send({files: [{ attachment: canvas.toBuffer(), name: "Account.png"}]});
+
+        message.channel.send({files: [{ attachment: canvas.toBuffer(), name: "Account.png"}]});
+    });
 }
 
 module.exports.help = {
