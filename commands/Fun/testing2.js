ca71d118f03baa4eb9e2a7eda1b5092fc8839c68
RHG <huskey.rusty@gmail.com>
2018-09-10 07:12:37 -0400

    Update testing2.js

--- commands/Fun/testing2.js
+++ commands/Fun/testing2.js
@@ -1,14 +1,16 @@
-const { MessageAttachment } = require('discord.js'); 
 const { Canvas } = require("canvas-constructor");
 
-const canvas = new Canvas(400,187)
-let serverSize = message.guild.memberCount;
-let canvas = new Canvas(x, y)
+module.exports.run = (bot, message, args) => {
+    const canvas = new Canvas(400,187)
+    let serverSize = message.guild.memberCount;
+    let canvas = new Canvas(400, 200)
+    .setColor("#000000")
+    .addRect(0, 0, 400, 200)
+    .setColor("#ffffff")
+    .addText(`Server Members: ${serverSize}`, 200, 100)
 
-.addRect(serverSize, 200, 130)
-
-
-
+    message.channel.send({files: [{ attachment: canvas.toBuffer(), name: "Account.png"}]});
+}
 
 module.exports.help = {
     name: "testing1",
