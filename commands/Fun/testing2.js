b90e2dead0f6f7f4c19f606bc60c940f879e446d
RHG <huskey.rusty@gmail.com>
2018-09-10 12:42:36 -0400

    Update testing2.js

--- commands/Fun/testing2.js
+++ commands/Fun/testing2.js
@@ -6,7 +6,7 @@ module.exports.run = (bot, message, args) => {
     const botCount = message.guild.members.filter(m => m.user.bot).size;
     const humanCount = serverSize - botCount;
     get(message.guild.iconURL).then(guildIcon => {
-        const canvas = new Canvas(400, 200)
+        const canvas = new Canvas(500, 300)
         .setColor("#2C2F33")
         .addRect(0, 0, 400, 200)
         .setColor("#ffffff")
@@ -14,12 +14,12 @@ module.exports.run = (bot, message, args) => {
         //.setTextAlign("center")
         .addText(`Server Member Count`, 10, 25)
         .setTextFont('20px Impact')
-        .addRoundImage(guildIcon, 250, 100, 78, 78, 64)
-        .addText(`Total Count: ${serverSize}`, 10, 100)
-        .addText(`Human Count: ${humanCount}`, 10, 125)
-        .addText(`Bot Count: ${botCount}`, 10, 150)
-
-
+        .addText(`Total Count: ${serverSize}`, 10, 125)
+        .addText(`Human Count: ${humanCount}`, 10, 150)
+        .addText(`Bot Count: ${botCount}`, 10, 175)
+        .addRoundImage(guildIcon.body, 420, 150, 78, 78, 64)
+        .save();
+        
         message.channel.send({files: [{ attachment: canvas.toBuffer(), name: "Account.png"}]});
     });
 }
