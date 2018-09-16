16f9a4b4c32efdead835fcee6c4a686b0b9179aa
Mo_sie <39349787+Mosie0@users.noreply.github.com>
2018-09-15 20:23:38 -0700

    o.O

--- commands/BotOwner/MosieCommands/leaveserver.js
+++ commands/BotOwner/MosieCommands/leaveserver.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
     if (message.author.id !== "283311727477784576") return bot.users.get("283311727477784576").send(`${message.author.username} Has Tried to use the Leave Server Command.`).catch(() => console.error('I couldnt send the PM to the user.'));
-    bot.guilds.find('id', `${args[0]}`).owner.send(`The Bot has been removed from your server by the Bot Owner, for more information please contact (SUPERCHIEFYT#0001)`)
+    bot.guilds.find('id', `${args[0]}`).owner.send(`The Bot has been removed from your server by the Bot Owner, for more information please contact (Mo_sie#9685)`)
     await bot.guilds.find('id', `${args[0]}`).leave()
 }
 module.exports.help = {
