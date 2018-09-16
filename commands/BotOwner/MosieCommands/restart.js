acfc6257ba43fbc99c97eac34824d595e753dd71
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2018-09-16 10:48:57 -0700

    Updated..

--- commands/BotOwner/MosieCommands/restart.js
+++ commands/BotOwner/MosieCommands/restart.js
@@ -5,20 +5,16 @@ module.exports.run = async (bot, message, args) => {
     let botembed = new Discord.RichEmbed()
     .setColor("#000FF")
     .addField("Restart Issued", `<@${message.author.id}> Has Restarted the Bot!`)
-    .setThumbnail(bot.user.avatarURL)
-    .setFooter("Command Ran By: " + message.author.username, message.author.avatarURL)
     .setTimestamp()
     console.log(`Bot Has Been Restart By: ` + message.author.username);
     if (message.author.id !== "283311727477784576" && message.author.id !== "288450828837322764") return;
     await message.react("✅");
     await message.channel.send(botembed);
     message.delete(1000).catch();
-    fs.writeFile('./log.json', JSON.stringify(`Bot has Been Restarted.`), (err) => {
-        if (err) console.log(err)
-    });
-    process.exit();
+    await bot.destroy().then(bot.login(process.env.BOT_TOKEN))
 
 }
 module.exports.help = {
-    name: "restart"
+    name: "restart",
+    names: "RT"
 }
