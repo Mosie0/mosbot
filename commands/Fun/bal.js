636b7a8f5bde5a1a5614e5a4d46508f35458dd84
RHG <huskey.rusty@gmail.com>
2018-09-16 11:52:39 -0400

    Create bal.js

---
+++ commands/Fun/bal.js
@@ -0,0 +1,18 @@
+// SOON ;)
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    let balEmbed = new Discord.RichEmbed()
+    .setAuthor(message.author.tag)
+    .setColor("#000FF")
+    .setDescription('SOON')
+
+
+    message.channel.send(balEmbed);
+
+}
+
+module.exports.help = {
+    name: "balance",
+    names: "bal"
+}
