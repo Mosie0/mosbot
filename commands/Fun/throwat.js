2419bdf5c31570ea261b5cf390987d6461cd8fa2
Mo_sie <39349787+Mosie0@users.noreply.github.com>
2018-09-16 16:57:26 -0700

    o.O

--- commands/Fun/throwat.js
+++ commands/Fun/throwat.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
+    let kUser = message.mentions.members.first() || message.guild.members.get(args[0]);
+    if (!kUser) return message.channel.send("Who do you want me to throw this at??");
     const throwuser = message.mentions.users.first() || message.author;
     let botmessage = args.slice(1).join(" ");
     let embed = new Discord.RichEmbed()
