2f8932f1919526eb0d48eddbe9b5a75cd6aef5ba
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2018-09-16 14:28:25 -0700

    updated

--- commands/Moderation/report.js
+++ commands/Moderation/report.js
@@ -1,17 +1,12 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-
-
     let rUser = message.guild.member(message.mentions.members.first() || message.guild.members.get(args[0]));
     if (!rUser) return message.channel.send("Couldn't find that user!");
-    let reason = args.join(" ").slice(22);
-
+    let reason = args.slice(1).join(" ");
+    if(!reason) return message.channel.send(`Please provide a valid reason to report the person for.`)
     let sIcon = message.guild.iconURL;
-
-
     let reportEmbed = new Discord.RichEmbed()
-
         .setTitle("User Report")
         .setColor("#2CE51A")
         .setThumbnail("https://cdn.discordapp.com/attachments/444028025932349441/445824984204705792/tenor_12.gif")
@@ -28,25 +23,12 @@ module.exports.run = async (bot, message, args) => {
         .setThumbnail("https://cdn.discordapp.com/attachments/434973905485037578/434976194622914561/user_report.gif")
         .addField("Reported User", `${rUser}`,true)
         .addField("Thank You", "Your Report has been given to the Moderators of the Server, They will get back to you shortly.")
-
-
-
-    let reportschannel = message.guild.channels.find(`name`, "modlogs");
+    let reportschannel = message.guild.channels.find(c => c.name === "modlogs");
     if (!reportschannel) return message.channel.send("Couldn't find modlogs");
-
     message.delete().catch();
-
     reportschannel.send(reportEmbed);
     message.author.send(dmEmbed);
-
-
-    if (!message.content.startsWith(prefix)) return;
-
-
-
 }
-
-
 module.exports.help = {
     name: "report"
 }
