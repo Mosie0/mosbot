3687241cf28163539ee8f4f9bf76c65c5cd00338
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2018-09-20 06:49:43 -0700

    fixed o_o

--- commands/Moderation/addrole.js
+++ commands/Moderation/addrole.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-
+const Settings = require('../../models/settings.js');
 module.exports.run = async (bot, message, args) => {
 
     //!addrole @andrew Dog Person
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
     if (!rMember) return message.reply("Sorry please check again i don't see that user.");
     let role = args.slice(1).join(" ");
     if (!role) return message.reply("Specify a role!");
-    let gRole = message.guild.roles.find(c => c === role);
+    let gRole = message.guild.roles.find(c => c.name === role) || message.guild.roles.find(c => c.id === role);
     if (!gRole) return message.reply("Couldn't find that role.");
 
     if (rMember.roles.has(gRole.id)) return message.reply("They already have that role.");
@@ -20,6 +20,15 @@ module.exports.run = async (bot, message, args) => {
         message.channel.send(`Congrats to <@${rMember.id}>, they have been given the role ${gRole.name}. I've Tried Dming the <@${rMember.id}> but their Dms are Closed.`)
     }
     message.delete().catch();
+      Settings.findOne({serverID: message.channel.guild.id}, (err, settings) => {
+        if (err) console.log(err);
+        if (settings) {
+         if (settings.logchannel == "") return;
+         let modlogs = message.guild.channels.get(settings.logchannel);
+        if (!modlogs) return;
+         modlogs.send(modlogsembed); 
+        }
+      });
 }
 
 module.exports.help = {
