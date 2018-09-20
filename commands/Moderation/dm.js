71e3fed805ddf327e9da1740bf76e7acc03c35e6
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2018-09-19 19:40:35 -0700

    .

--- commands/Moderation/dm.js
+++ commands/Moderation/dm.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const Settings = require('../../modules/settings.js');
+const Settings = require('../../models/settings.js');
 module.exports.run = async (bot, message, args) => {
     let nopermembed = new Discord.RichEmbed()
     .setColor("FF0000")
