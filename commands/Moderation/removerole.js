16eae3e23c3444b7986e633054a08fd8e596762d
Mo_sie <39349787+Mosie0@users.noreply.github.com>
2018-09-19 20:53:02 -0700

    oH yES

--- commands/Moderation/removerole.js
+++ commands/Moderation/removerole.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const Settings = require('../../models/settings.js');
 
 module.exports.run = async (bot, message, args) => {
     if (!message.member.hasPermission("MANAGE_ROLES")) return message.reply("Sorry pal, you can't do that.");
@@ -12,12 +13,27 @@ module.exports.run = async (bot, message, args) => {
     if (!rMember.roles.has(gRole.id)) return message.reply("They don't have that role.");
     await (rMember.removeRole(gRole.id));
 
+    let modlogsembed = new Discord.RichEmbed()
+    .setColor("FF0000")
+    .addField(`Member Involved <@${rMember.id}>`)
+    .addField(`Role Lost: ${gRole.name}`)
+
     try {
         await rMember.send(`RIP, you lost the ${gRole.name} role.`)
     } catch (e) {
         message.channel.send(`<@${rMember.id}>, Sorry but you ware removed from ${gRole.name} role I've Tried Dming the <@${rMember.id}> but their Dms are Closed.`)
     }
     message.delete().catch();
+
+    Settings.findOne({serverID: message.channel.guild.id}, (err, settings) => {
+        if (err) console.log(err);
+        if (settings) {
+         if (settings.logchannel == "") return;
+         let modlogs = message.guild.channels.get(settings.logchannel);
+        if (!modlogs) return;
+         modlogs.send(modlogsembed); 
+        }
+      });
 }
 
 module.exports.help = {
