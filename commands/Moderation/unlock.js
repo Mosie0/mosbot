44e6b7f5628b3df6c6e2614e66820f105a3d4999
Mo_sie <39349787+Mosie0@users.noreply.github.com>
2018-09-20 19:13:05 -0700

    o.o

--- commands/Moderation/unlock.js
+++ commands/Moderation/unlock.js
@@ -1,9 +1,11 @@
 const Discord = require('discord.js');
 const ms = require('ms');
+const Settings = require('../../models/settings.js');
 module.exports.run = async (bot, message, args) => {
     const nopermembed = new Discord.RichEmbed()
     .setColor(`#FF0000`)
     .setDescription(`<@${message.author.id}> You Don't have the Manage Messages Permission!`)
+    let Moderatoruser = message.author.id;
     if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(nopermembed);
     message.channel.overwritePermissions(message.guild.id, {
         SEND_MESSAGES: true,
@@ -13,7 +15,23 @@ module.exports.run = async (bot, message, args) => {
         .setColor(`#FF000`)
         .setDescription(`<@${message.author.id}> This Channel is now Unlocked!`)
     message.channel.send(lockembed)
-}
+
+    Settings.findOne({serverID: message.channel.guild.id}, (err, settings) => {
+        if (err) console.log(err);
+        if (settings) {
+         if (settings.logchannel == "") return;
+         let modlogs = message.guild.channels.get(settings.logchannel);
+        if (!modlogs) return;
+    let modlogsembed = new Discord.RichEmbed()
+        .setColor(`#FF0000`)
+        .addField(`UNLOCKED`, `CHANNEL: <#${message.channel.id}> `)
+        .addField(`Moderator:`, `<@${Moderatoruser}>`)
+        .setTimestamp()
+         modlogs.send(modlogsembed); 
+    
+        }
+      });
+    }
 module.exports.help = {
     name: "unlock"
 }
