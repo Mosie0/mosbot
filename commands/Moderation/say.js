5dec997779f56e4244326dbefcb54c4555e46d21
RHG <huskey.rusty@gmail.com>
2018-09-21 22:13:40 -0400

    Dat fix

--- commands/Moderation/say.js
+++ commands/Moderation/say.js
@@ -1,10 +1,10 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
-    let nopermembed = new Discord.RichEmbed()
-        .setColor("FF0000")
-        .setDescription(`${message.author}, you do not have the MANAGE_MESSAGES permission.`)
-    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(nopermembed)
+    //let nopermembed = new Discord.RichEmbed()
+        //.setColor("FF0000")
+        //.setDescription(`${message.author}, you do not have the MANAGE_MESSAGES permission.`)
+    //if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(nopermembed)
     let channels = message.mentions.channels.first() || message.guild.channels.get(args[0])
     if(!channels) channels = message.channel.send(args.join(' '))
     let botmessage = args.slice(1).join(" ");
@@ -14,7 +14,7 @@ module.exports.run = async (bot, message, args) => {
 
 
 module.exports.help = {
-    perm: "creator",
+    perm: "admins",
     name: "say",
     names: "Say"
 }
