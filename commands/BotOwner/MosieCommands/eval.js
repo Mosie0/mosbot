c888879d6940a29ee6c523d6b473de91941c57ab
Mo_sie <39349787+Mosie0@users.noreply.github.com>
2018-09-21 20:59:56 -0700

    0.0

--- commands/BotOwner/MosieCommands/eval.js
+++ commands/BotOwner/MosieCommands/eval.js
@@ -8,8 +8,8 @@ module.exports.run = async (bot, message, args, ops, ops2) => {
 
         if (typeof evaled !== "string")
             evaled = require("util").inspect(evaled);
-        if (evaled.includes(bot.token)) evaled = evaled.replace(bot.token, 'TOKEN GOES HERE');
-        if (evaled.includes(process.env.passmongodb)) evaled = evaled.replace(process.env.passmongodb, 'MONGO DB PASSWORD GOES HERE');
+        if (evaled.includes(bot.token)) evaled = evaled.replace(bot.token, 'NO THATS MY BOT TOKEN :rage:');
+        if (evaled.includes(process.env.passmongodb)) evaled = evaled.replace(process.env.passmongodb, 'r u trying to get my mongo password :angry:');
         let cleanembed = new Discord.RichEmbed()
             .setColor("#000FF")
             .setDescription(`📥Input\n\n${code}\n\n📤Output\n\n${clean(evaled)}`)
