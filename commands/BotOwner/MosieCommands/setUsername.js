83870ca169dd8715efa33a58eb63f850773566a8
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2018-09-23 13:54:10 -0700

    fixed

--- commands/BotOwner/MosieCommands/setUsername.js
+++ commands/BotOwner/MosieCommands/setUsername.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    if (message.author.id !== "283311727477784576" && message.author.id !== "288450828837322764") return;
     let newbotname = args.join(" ");
     if (!newbotname) return message.channel.send(`Sorry But you need to provide a new Name!!`)
     bot.user.setUsername(newbotname);
@@ -9,5 +8,6 @@ module.exports.run = async (bot, message, args) => {
 }
 
 module.exports.help = {
-    name: "setname"
+    name: "setname",
+    perm: "creator"
 }
