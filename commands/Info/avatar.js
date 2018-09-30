ecca21a4369e9cc0377e0f71265f951f316dcc48
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2018-09-30 00:30:23 -0700

    updated o.o

--- commands/Info/avatar.js
+++ commands/Info/avatar.js
@@ -1,18 +1,30 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    let aTaged = message.mentions.users.first();
-    if (!aTaged) {
-        return message.channel.send(`Please Provide a User :wink:` + "\n **m!avatar @user**");
-    }
-    let botembed = new Discord.RichEmbed()
-        .setColor("#000FF")
-        .setImage(`${aTaged.displayAvatarURL}`)
-        .setAuthor(`${aTaged.username}` , `${aTaged.displayAvatarURL}`)
-        .setFooter("Command Ran By: " + message.author.username, message.author.displayAvatarURL);
-    message.channel.send(botembed);   
+    let user = message.mentions.users.first() || message.author
+     if(user.displayAvatarURL.includes(".png" || ".jpg")){
+            let embed = new Discord.RichEmbed()
+                .setColor(`RANDOM`)
+                .setAuthor(user.tag, user.displayAvatarURL)
+                .setDescription(`[Avatar URL](https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.jpg?size=2048)`)
+                .setImage(`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.jpg?size=2048`)
+                .setFooter(`Requested By: ${message.author.tag}`, message.author.displayAvatarURL)
+            message.channel.send(embed)
+        } else 
+            if (user.displayAvatarURL.includes(".gif")) {
+                let embed = new Discord.RichEmbed()
+                    .setColor(`RANDOM`)
+                    .setAuthor(user.tag, user.displayAvatarURL)
+                    .setDescription(`[Avatar URL](https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.gif?size=2048)`)
+                    .setImage(`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.gif?size=2048`)
+                    .setFooter(`Requested By: ${message.author.tag}`, message.author.displayAvatarURL)
+                message.channel.send(embed)
+            } else {
+                return;
+            }
 }
 module.exports.help = {
     perm: "all",
-    name: "avatar"
+    name: "avatar",
+    names: "Avatar"
 };
