61c9aa031e48889074723a227706e2cf173bec7e
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2018-10-23 01:27:16 -0700

    Update roles.js

--- commands/Info/roles.js
+++ commands/Info/roles.js
@@ -1,5 +1,52 @@
-module.exports.run = async (bot, message, args) => {
-    message.channel.send(`Server Roles ${message.guild.roles.map(role => role.name).join(' , ')}`);
+module.exports.run = async (bot, msg, args) => {
+     let guild = msg.guild;
+        let fields = []
+        let roles = {
+            0: []
+        }
+        let counter = 0
+        guild.roles.forEach((role, index) => {
+            if (roles[counter].join('\n').length > 950) {
+                if (++index === guild.roles.length) {
+                    roles[counter].push(`${role}`)
+                } else {
+                    counter++
+                    roles[counter] = []
+                }
+            } else {
+                roles[counter].push(`${role}`)
+            }
+        })
+        if (roles[0].join('').length !== 0) {
+            Object.keys(roles).forEach((collection, index) => {
+                if (index !== 0) {
+                    fields.push({
+                        name: 'More',
+                        value: roles[index].join('\n')
+                    })
+                } else {
+                    fields.push({
+                        name: `Roles [${guild.roles.size}]`,
+                        value: roles[index].join('\n')
+                    })
+                }
+            })
+        } else {
+            fields.push({
+                name: 'Roles',
+                value: '**None**'
+            })
+        }
+        msg.channel.send({
+            embed: {
+                timestamp: new Date(msg.timestamp),
+                color: msg.guild.me.hoistRole.color,
+                thumbnail: {
+                    url: guild.iconURL ? guild.iconURL : `http://www.kalahandi.info/wp-content/uploads/2016/05/sorry-image-not-available.png`
+                },
+                fields: fields
+            }
+        }).catch(() => { })
 }
 
 module.exports.help = {
