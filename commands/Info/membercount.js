ea503f5f0c2668282f7ae4f7bc4c5f2006d4be22
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2018-10-25 19:17:32 -0700

    Update membercount.js

--- commands/Info/membercount.js
+++ commands/Info/membercount.js
@@ -1,15 +1,16 @@
 const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
-    let serverSize = message.guild.memberCount;
-    let botCount = message.guild.members.filter(m => m.user.bot).size;
-    let humanCount = serverSize - botCount;
-const embed = new Discord.RichEmbed()
-.setColor(`RANDOM`)
-.setTimestamp()
-.addField(`Members`, `**${serverSize}**`, true)
-.addField(`Humans`, `**${humanCount}**`, true)
-.addField(`Bots`, `**${botCount}**`, true)
-message.channel.send(embed)
+    let serverSize = await message.guild.memberCount;
+        let botCount = await message.guild.members.filter(m => m.user.bot).size;
+        let humanCount = await serverSize - botCount;
+        const embed = new Discord.RichEmbed()
+            .setAuthor(message.guild.name, message.guild.iconURL ? message.guild.iconURL : "https://cdn.discordapp.com/emojis/483118381650804747.gif")
+            .setColor(`RANDOM`)
+            .setTimestamp()
+            .addField(`Members`, `**${serverSize}**`, true)
+            .addField(`Humans`, `**${humanCount}**`, true)
+            .addField(`Bots`, `**${botCount}**`, true)
+        message.channel.send(embed)
 }
 module.exports.help = {
     perm: "all",
