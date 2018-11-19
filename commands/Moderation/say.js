54d011a59c9bf94913a6479e124bf2ec7c77c5a3
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2018-11-19 08:52:39 -0800

    Update say.js

--- commands/Moderation/say.js
+++ commands/Moderation/say.js
@@ -1,15 +1,10 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
-    //let nopermembed = new Discord.RichEmbed()
-        //.setColor("FF0000")
-        //.setDescription(`${message.author}, you do not have the MANAGE_MESSAGES permission.`)
-    //if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(nopermembed)
     let channels = message.mentions.channels.first() || message.guild.channels.get(args[0])
     if(!channels) channels = message.channel.send(args.join(' '))
-    let botmessage = args.slice(1).join(" ");
     message.delete().catch();
-    message.channel.send(botmessage);
+    channels.send(args.slice(1).join(" "));
 }
 
 
