366df2c3712d02fc335feaefcd8a6e7d7138a551
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2019-01-06 12:45:37 -0800

    Update Dog.js

--- commands/Fun/Dog.js
+++ commands/Fun/Dog.js
@@ -2,21 +2,19 @@ const Discord = require("discord.js");
 const superagent = require("superagent");
 
 module.exports.run = async (bot,message,args) => {
-    const userURL = message.author.avatarURL;
-    const usernameid = message.author.username;
-    let { body } = await superagent
-        .get(`https://random.dog/woof.json`);
-        let embed = new Discord.RichEmbed()
-        .setColor("#FF000")
-        .setDescription("<a:Dots:426956230582599690> Loading the Command, Please Wait.,,,,")
+      let { body } = await superagent.get(`https://dog.ceo/api/breeds/image/random`);
+        if (body.status === "success") {
+            let embed = new Discord.RichEmbed()
+                .setColor("#FF000")
+                .setDescription("<a:Dots:426956230582599690> Loading the Photo..")
 
-        message.channel.send(embed).then(message => {
-            embed.setColor("#000FF")
-            embed.setDescription("Here's a Photo of a Dog 😊")
-            embed.setImage(body.url)
-            embed.setFooter("Command Ran By: " + usernameid, userURL)
-            message.edit(embed)
-        })
+            message.channel.send(embed).then(message => {
+                embed.setColor("RANDOM")
+                embed.setDescription("Here's a Photo of a Dog 😊")
+                embed.setImage(body.message)
+                message.edit(embed)
+            })
+        } 
 }
 
 module.exports.help = {
