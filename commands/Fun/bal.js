1558f6cc5b2c8f36883cd05118676d7b361d249a
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2019-01-10 19:47:35 -0800

    Update bal.js

--- commands/Fun/bal.js
+++ commands/Fun/bal.js
@@ -1,15 +1,21 @@
 // SOON ;)
 const Discord = require("discord.js");
-
+const idk = require("../../models/money.js");
 module.exports.run = async (bot, message, args) => {
-    let balEmbed = new Discord.RichEmbed()
-    .setAuthor(message.author.tag)
-    .setColor("#000FF")
-    .setDescription('SOON')
-
-
-    message.channel.send(balEmbed);
-
+   idk.findOne({guildID: message.guild.id, userID: message.author.id}, async (err, db) => {
+    if(!db){return message.channel.send(`${message.author}, I couldn't find anything for you in our database!`)}else{
+    let e = new Discord.RichEmbed()
+    .setColor("RANDOM")
+    .setAuthor(message.author.tag, message.author.displayAvatarURL)
+    .setTimestamp()
+    .addField("Money", `$${db.money}`, true)
+    .addField("XP", `${db.xp}`, true)
+    .addField("Level", `${db.level}`, true)
+    .addField("Next Level", `${db.nextLevel}`, true)
+    .setFooter("Note: This isn't fully added yet >.>", bot.user.displayAvatarURL)
+    return message.channel.send(e)
+    }
+})
 }
 
 module.exports.help = {
