c21dd6304536c43d9f6a03c7bbe64e2a35ce0e0a
SUPERCHIEFYT <34470712+SUPERCHIEFYT@users.noreply.github.com>
2019-01-12 14:31:53 -0800

    Update ping!.js

--- commands/Info/ping!.js
+++ commands/Info/ping!.js
@@ -1,19 +1,21 @@
 const Discord = require("discord.js");
-
+const moment = require("moment");
 module.exports.run = async (bot, message, args) => {
-    const useruser = "Command Ran By: " + message.author.username;
-    const userurl = message.author.avatarURL;
-    let botembed = new Discord.RichEmbed()
-        .setColor("#000FF")
-        .setDescription(`<a:Dots:426956230582599690> Loading......`)
-        .setTimestamp()
-    message.channel.send(botembed).then(message =>{
-        botembed.setColor("#000FF")
-        botembed.setDescription(`:ping_pong: Pong! **\`${bot.pings[0]}ms\`**`)
-        botembed.setFooter(useruser, userurl)
-        botembed.setTimestamp()
-        message.edit(botembed)
-    })
+        let loadingembed = new Discord.RichEmbed()
+            .setColor(`RANDOM`)
+            .setDescription(`Loading..`)
+            .setTimestamp()
+        const m = await message.channel.send(loadingembed);
+        let embed = new Discord.RichEmbed()
+            .setColor(`RANDOM`)
+            .setTitle(`<:Mosbot:533775017435987979> Status <:Mosbot:533775017435987979>`)
+            .setFooter(message.author.tag, message.author.displayAvatarURL)
+            .addField(`Message Latency`, `${message.createdTimestamp - m.createdTimestamp}ms`, true)
+            .addField(`API Latency`, `${Math.round(bot.ping)}ms`, true)
+            .addField(`Uptime`, `${moment.duration(bot.uptime).format(" D [days], H [hrs], m [mins], s [secs]")}`, true)
+            .setAuthor(bot.user.tag, bot.user.displayAvatarURL)
+        m.edit(embed);
+
 
 }
 module.exports.help = {
